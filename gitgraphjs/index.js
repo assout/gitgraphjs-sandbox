4a32f372782ba967f50704dd8355ee906272d5ce
assout <assout@users.noreply.github.com>
2016-11-23 13:56:58 +0900

    WIP:  gitgraphjs/index.html |  3 ++-  gitgraphjs/index.js   | 23 ++++++++++++++++++++++-  2 files changed, 24 insertions(+), 2 deletions(-)

--- gitgraphjs/index.js
+++ gitgraphjs/index.js
@@ -1,4 +1,25 @@
-var gitgraph = new GitGraph();
+var gitgraph = new GitGraph({
+  elementId: "first"
+});
+
+// masterブランチを作成 ＆ HEADを切り替えて2回コミット
+var master = gitgraph.branch('master');
+gitgraph.commit().commit();
+
+// developブランチを作成 ＆ HEADを切り替えて2回コミット
+var develop = gitgraph.branch('develop_porin');
+gitgraph.commit().commit();
+
+// HEADをmasterブランチに切り替えて1回コミット
+master.checkout();
+gitgraph.commit();
+
+// developブランチをmasterブランチにマージ
+develop.merge(master);
+
+var gitgraph = new GitGraph({
+  elementId: "seconds"
+});
 
 // masterブランチを作成 ＆ HEADを切り替えて2回コミット
 var master = gitgraph.branch('master');
